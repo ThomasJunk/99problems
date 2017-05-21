e2ce80ddff1a9be61175204d590f7ed424f074d5
Thomas Junk <tjunk@intevation.de>
2017-05-21 14:44:50 +0200

    problem6

--- problems/lib/problems.ex
+++ problems/lib/problems.ex
@@ -2,9 +2,9 @@ defmodule Problems.CLI do
   @moduledoc """
   Documentation for Problems.
   """
-  import Problem5
+  import Problem6
 
   def main(_) do
-    IO.inspect(rev([1,2,3,4,5]))
+    IO.inspect(is_palindrome('OTTO'))
   end
 end
