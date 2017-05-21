a8407ef20f59faccfef38347dd4131b506c0e7c3
Thomas Junk <tjunk@intevation.de>
2017-05-21 19:19:26 +0200

    problem11

--- problems/lib/problems.ex
+++ problems/lib/problems.ex
@@ -2,11 +2,11 @@ defmodule Problems.CLI do
   @moduledoc """
   Documentation for Problems.
   """
-  import Problem10
+  import Problem11
 
   def main(_) do
     [1,2,2,2,2,2,2,3,4,5,5,5,6,6,6,7]
-    |> enc
+    |> enc_mod
     |> IO.inspect
   end
 
